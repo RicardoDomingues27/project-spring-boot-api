aee5b6c3f224a67444b4e6888bb2ec2fbc01d716
Ricardo Domingues <ricardo.domingues27@gmail.com>
2020-11-04 12:20:56 -0300

    Updade Dependences pom.xml And Add Dto File

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -6,21 +6,22 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 
-@Controller
+@RestController
 public class TopicosController {
 
 	@RequestMapping("/topicos")
-	@ResponseBody
-	public List<Topico> lista(){
+	public List<TopicoDto> lista(){
 		Curso curso = new Curso("Spring Boot", "Programacao");
 		Topico topico =  new Topico("Duvida Spring", "Sobre o Curso de Spring", curso);
 		
 		
 		
-		return Arrays.asList(topico,topico,topico);
+		return Arrays.asList(new TopicoDto(topico));
 	}
 }
